d50d7d801b47e53df2cd900350e9aa4ac17481fd
cslewislives <cslewislives@gmail.com>
2017-12-12 01:37:52 -0500

    Pseudo-code and basic html

---
+++ assets/javascript/game.js
@@ -0,0 +1,52 @@
+//initialize game
+// Variables:
+// HP, AP, CAP, Lock?
+
+//========================================================
+    // object characters {
+        
+        //Each character has HP, AP and CAP
+        //Each character must differ
+
+    // }
+
+    //If (player chooses a character) {
+
+        //player is that character for the rest of the game
+        //other character choices are moved to the enemies section
+
+    // }
+//player chooses a character from options
+    //Minimum of 4 options
+        //HP, Sirius, Voldemort, Malfoy
+
+//========================================================
+    // If (player chooses enemy) {
+        
+        //enemy moves to defender area
+
+    // }
+    //NPC has HP and CAP
+
+//========================================================
+    // if (PC clicks Attack) {
+        
+        //characters deal damage simultaneously
+        //PC damage increases with each hit
+        //NPC counter attack stays the same
+
+    // }
+
+    //If (NPC HP === 0) {
+        
+        //when NPC HP is reduced to 0 NPC is removed from the game
+    
+    // } 
+    
+    //PC loses when HP goes to 0
+
+//========================================================
+
+
+//PC can now choose new opponent
+//PC wins by defeating all opponents
